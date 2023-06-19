e5072dcf9e47a97273c0088461db808ca94c7529
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-06-19 16:48:05 +0200

    entering Calculator class

---
+++ src/main/java/org/lessons/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package org.lessons.java.calculator;
+
+public class Calculator {
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new IllegalArgumentException("Non si può dividere per 0!");
+        }
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+}
